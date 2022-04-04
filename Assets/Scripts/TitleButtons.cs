e0297502eb5331e5defd87d5fd48fdb84a61cd6c
coolguy284 <coolguy284v64@gmail.com>
2022-04-03 21:07:18 -0400

    Finish implementing basic menus

--- Assets/Scripts/TitleButtons.cs
+++ Assets/Scripts/TitleButtons.cs
@@ -4,11 +4,23 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class TitleButtons : MonoBehaviour {
+    public GameObject MainMenu;
+    public GameObject OptionsMenu;
+
     public void StartGame() {
         SceneManager.LoadScene("Levels");
     }
     
     public void QuitGame() {
         Application.Quit();
+    }
+
+    void Update() {
+        if (Input.GetButtonDown("Cancel")) {
+            if (OptionsMenu.activeSelf) {
+                MainMenu.SetActive(true);
+                OptionsMenu.SetActive(false);
+            }
+        }
     }
 }
