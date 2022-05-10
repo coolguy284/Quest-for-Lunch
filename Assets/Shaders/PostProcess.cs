f69fa263144a27d4687ee30c3f8960beff062601
coolguy284 <coolguy284v64@gmail.com>
2022-05-09 20:23:36 -0400

    Fix platform ledge bug, use material instances

--- Assets/Shaders/PostProcess.cs
+++ Assets/Shaders/PostProcess.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class PostProcess : MonoBehaviour {
     public Material material;
+    Material materialInst;
     public bool vignette = true;
     public bool psychedelic = false;
     public bool bowl = false;
 
+    void Awake() {
+        materialInst = new Material(material);
+    }
+
     void OnRenderImage(RenderTexture src, RenderTexture dest) {
-        material.SetFloat("_vignette", vignette ? 1.0f : 0.0f);
-        material.SetFloat("_psychedelic", psychedelic ? 1.0f : 0.0f);
-        material.SetFloat("_bowl", bowl ? 1.0f : 0.0f);
-        Graphics.Blit(src, dest, material);
+        materialInst.SetFloat("_vignette", vignette ? 1.0f : 0.0f);
+        materialInst.SetFloat("_psychedelic", psychedelic ? 1.0f : 0.0f);
+        materialInst.SetFloat("_bowl", bowl ? 1.0f : 0.0f);
+        Graphics.Blit(src, dest, materialInst);
     }
 }
